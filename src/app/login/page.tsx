0ad3371ee37b2851e5e52aa96e2d8c060fb5e04d
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-06-27 15:45:59 -0500

    feat(login): client responds to api endpoint for login in now

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/LoginForm";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(10, {
+      message: "Password is invalid",
+    })
+    .regex(new RegExp(".*[0-9].*"), {
+      message: "Password is invalid",
+    }),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof loginSchema>) => {
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "Application/JSON" },
+        body: JSON.stringify({
+          email: values.email,
+          password: values.password,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorBody = await response.json();
+        throw new Error(errorBody.message || "Login failed");
+      }
+
+      const res = await response.json();
+      console.log("🛠 API response:", res);
+    } catch (error) {
+      console.log("try block failed error is: ", error);
+    }
+
+    //jump to another route later
+  };
+
+  return (
+    <div className="bg-black p-8 rounded-lg w-96 border border-gray-600">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-white font-bold">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="you@example.com"
+                    {...field}
+                    className="bg-black border border-gray-600 text-white"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-white font-bold">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="**********"
+                    {...field}
+                    className="bg-black border border-gray-600 text-white"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="w-full bg-white text-black hover:bg-gray-200 font-bold"
+          >
+            Create Account
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
