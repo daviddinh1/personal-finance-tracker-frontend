b7a5371fc99447702d131e382004c68498b4e17d
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-07-02 18:33:15 -0500

    feat(transaction): transaction hovers over the chart now for a cleaner UI

--- src/components/AddTransactions.tsx
+++ src/components/AddTransactions.tsx
@@ -79,85 +79,92 @@ export default function AddTransactions({
     }
   };
   return (
-    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-      <CollapsibleTrigger asChild>
-        <Button variant="outline">+ Add Transaction</Button>
-      </CollapsibleTrigger>
-      <CollapsibleContent>
-        <div className="bg-neutral-900 p-8 rounded-lg w-96 border border-neutral-600">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-                control={form.control}
-                name="amount"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-white font-bold">
-                      Amount
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        placeholder="Ex. 100.00"
-                        {...field}
-                        className="bg-black border border-gray-600 text-white"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="type"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-white fond-bold">Type</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="Type"
-                        placeholder="Ex. INCOME"
-                        {...field}
-                        className="bg-black border border-gray-600 text-white"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-white font-bold">
-                      Description
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="text"
-                        placeholder="Ex. Grocery shopping"
-                        {...field}
-                        className="bg-black border border-gray-600 text-white"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <Button
-                type="submit"
-                className="w-full bg-white text-black hover:bg-gray-200 font-bold"
+    <div className="relative">
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger asChild>
+          <Button variant="outline">+ Add Transaction</Button>
+        </CollapsibleTrigger>
+        <CollapsibleContent className="absolute z-50">
+          <div className="bg-neutral-900 p-8 rounded-lg w-96 border border-neutral-600">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-6"
               >
-                Enter Transaction
-              </Button>
-            </form>
-          </Form>
-        </div>
-      </CollapsibleContent>
-    </Collapsible>
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-white font-bold">
+                        Amount
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          placeholder="Ex. 100.00"
+                          {...field}
+                          className="bg-black border border-gray-600 text-white"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="type"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-white fond-bold">
+                        Type
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="Type"
+                          placeholder="Ex. INCOME"
+                          {...field}
+                          className="bg-black border border-gray-600 text-white"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-white font-bold">
+                        Description
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          placeholder="Ex. Grocery shopping"
+                          {...field}
+                          className="bg-black border border-gray-600 text-white"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button
+                  type="submit"
+                  className="w-full bg-white text-black hover:bg-gray-200 font-bold"
+                >
+                  Enter Transaction
+                </Button>
+              </form>
+            </Form>
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
   );
 }
